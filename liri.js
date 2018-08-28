aa50f102c03f22b3f9615f5cc4a54418b5bbbfaf
PeterMacDonnell <petermacdonnell@berkeley.edu>
2018-08-27 23:03:50 -0700

    homework submission

--- liri.js
+++ liri.js
@@ -1,6 +1,7 @@
 
 
-
+//npms and other linked files
+var fs = require('fs');
 var dotenv = require("dotenv").config();
 var keys = require("./keys.js");
 var request = require('request');
@@ -10,49 +11,73 @@ var spotify = new Spotify(keys.spotify);
 
 
 
-
+//user command line inputs
 var commandInput = process.argv[2];
 var item = process.argv[3];
 
 
 
-
+//the function for omdb api functionality
 var movieFunction = function () {
   var queryUrl = "http://www.omdbapi.com/?t=" + item + "&y=&plot=short&apikey=trilogy";
 
   request(queryUrl, function (error, response, body) {
     if(!error && response.statusCode === 200){
-    console.log(JSON.parse(body).Year, JSON.parse(body).Ratings[1]);
-    // console.log();
+    console.log(JSON.parse(body).Title, JSON.parse(body).Year, JSON.parse(body).Ratings[0], JSON.parse(body).Ratings[1], JSON.parse(body).Country, JSON.parse(body).Language, JSON.parse(body).Plot, JSON.parse(body).Actors);
+  
     }
   });
 
 };
 
+//the function for Spotify NPM functionality
 var spotifyFunction = function(){
   spotify.search({ type: 'track', query: item }, function(err, data) {
     if (err) {
       return console.log('Error occurred: ' + err);
     }
-   
-  console.log(data); 
+    var response = data.tracks.items;
+    response.forEach(song => {
+      console.log(song.name)
+      console.log(song.artists[0].name) 
+      console.log(song.album.name)
+      console.log(song.href)
+  
+    })
+  
   });
 
 
 };
 
+//function for bandsintown api functionality
 var concertFunction = function(){
   var queryUrl = "https://rest.bandsintown.com/artists/" + item + "/events?app_id=codingbootcamp";
 
-  request(queryUrl, function (body){
-    console.log(JSON.stringify(body, null, 2));
+  request(queryUrl, function (error, response, body){
+    if (!error && response.statusCode === 200) {
+      var bandArray = JSON.parse(body)
+      bandArray.forEach(function(event){
+          console.log(event.venue.name)
+          console.log(event.venue.city)
+          console.log(event.datetime)
+          // This will only send "this is loaded" to console I don't know why :'(
+          // console.log(event.venue.name, event.venue.city, event.venue.datetime)
+      });
+    };
   });
+  };
 
-
-
+  //function for reading from text file
+  var randomText = function(){
+    fs.readFile('random.txt', 'utf8', function(err, data){
+        data = data.split(',');
+        console.log(data)
+    })
 };
 
 
+//selects for apporpiate function execution depending on user's command input
 if(commandInput === "movie-this"){
     movieFunction();
 }
@@ -63,6 +88,10 @@ else if(commandInput === "conert-this"){
 
 else if(commandInput === "spotify-this-song"){
   spotifyFunction();
+}
+
+else if(commandInput === "do-what-it-says"){
+  randomText();
 };
 
 
@@ -71,4 +100,3 @@ else if(commandInput === "spotify-this-song"){
 //Spotify API info
 // Client ID 82feda22363241bb8acda145c2346bc5
 // Client Secret a300163c2ad54424a8577af447886949
-// var spotify = new Spotify(keys.spotify);
